b6eb640596375be44f4af15cbac3a61bdb91f63b
Michał <michal.szczepaniak1999@gmail.com>
2019-03-30 20:11:15 +0100

    New game, functional

--- scripts/vue.js
+++ scripts/vue.js
@@ -22,29 +22,76 @@ function move(i) {
             squareClass = ("empty" + i);
             squareClasses[i - 1] = [document.getElementById(squareClass).className];
         }
-        // alert(squareClasses[0] + squareClasses[1] + squareClasses[2] + squareClasses[3]+ squareClasses[4] + squareClasses[5] + squareClasses[6] + squareClasses[7] + "8" + squareClasses[8])
         
-        for (var i = 0; i < 9; i += 3){
+        var won = 0;
+        for (var i = 0; i < 9 && !won; i += 3){
             if ((squareClasses[i] == "xSquare" && squareClasses[i+1] == "xSquare" && squareClasses[i+2] == "xSquare") || (squareClasses[i] == "oSquare" && squareClasses[i+1] == "oSquare" && squareClasses[i+2] == "oSquare")) {
-                if (squareClasses[i] == "xSquare") setTimeout(function(){ alert("Krzyżyk wygrał!\nOdśwież by zagrać ponownie."); }, 20);
-                else if (squareClasses[i] == "oSquare") setTimeout(function(){ alert("Kółko wygrało!\nOdśwież by zagrać ponownie."); }, 20);
+                if (squareClasses[i] == "xSquare") xWon();
+                else if (squareClasses[i] == "oSquare") oWon();
+                won = 1;
             }
         }
-        for (var i = 0; i < 3; i++) {
+        for (var i = 0; i < 3 && !won; i++) {
             if ((squareClasses[i] == "xSquare" && squareClasses[i+3] == "xSquare" && squareClasses[i+6] == "xSquare") || (squareClasses[i] == "oSquare" && squareClasses[i+3] == "oSquare" && squareClasses[i+6] == "oSquare")) {
-                if (squareClasses[i] == "xSquare") setTimeout(function(){ alert("Krzyżyk wygrał!\nOdśwież by zagrać ponownie."); }, 20);
-                else if (squareClasses[i] == "oSquare") setTimeout(function(){ alert("Kółko wygrało!\nOdśwież by zagrać ponownie."); }, 20);
+                if (squareClasses[i] == "xSquare") xWon();
+                else if (squareClasses[i] == "oSquare") oWon();
+                won = 1;
             }
         }
-        if ((squareClasses[0] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[8] == "xSquare") || (squareClasses[0] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[8] == "oSquare")) {
-            if (squareClasses[0] == "xSquare") setTimeout(function(){ alert("Krzyżyk wygrał!\nOdśwież by zagrać ponownie."); }, 20);
-            else if (squareClasses[0] == "oSquare") setTimeout(function(){ alert("Kółko wygrało!\nOdśwież by zagrać ponownie."); }, 20);
+        if ((!won && squareClasses[0] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[8] == "xSquare") || (squareClasses[0] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[8] == "oSquare")) {
+            if (squareClasses[0] == "xSquare") xWon();
+            else if (squareClasses[0] == "oSquare") oWon();
+            won = 1;
         }
-        else if ((squareClasses[2] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[6] == "xSquare") || (squareClasses[2] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[6] == "oSquare")) {
-            if (squareClasses[2] == "xSquare") setTimeout(function(){ alert("Krzyżyk wygrał!\nOdśwież by zagrać ponownie."); }, 20);
-            else if (squareClasses[2] == "oSquare") setTimeout(function(){ alert("Kółko wygrało!\nOdśwież by zagrać ponownie."); }, 20);
+        else if ((!won && squareClasses[2] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[6] == "xSquare") || (squareClasses[2] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[6] == "oSquare")) {
+            if (squareClasses[2] == "xSquare") xWon();
+            else if (squareClasses[2] == "oSquare") oWon();
+            won = 1;
         }
 
-        //dwa przekreslenia na raz
+        if(counter == 9 && !won) draw();
     }
+
+}
+
+function newGame() {
+    for (var i = 1; i <= 9; i++){
+        var squareClass = ("empty" + i);
+        document.getElementById(squareClass).className = "emptySquare";
+        var buttonClass = document.getElementById(squareClass).className;
+        visited[i-1] = 0;
+    }
+    counter = 0;
+
+    document.getElementById("draw").className = "hidden";
+    var buttonClass = document.getElementById(squareClass).className;
+
+    document.getElementById("xWon").className = "hidden";
+    var buttonClass = document.getElementById(squareClass).className;
+
+    document.getElementById("oWon").className = "hidden";
+    var buttonClass = document.getElementById(squareClass).className;
+}
+
+function draw() {
+    document.getElementById("draw").className = "message";
+    var buttonClass = document.getElementById(squareClass).className;
+}
+
+function xWon() {
+    for (var i = 0; i < 9; i++) {
+        visited[i] = 1;
+    }
+
+    document.getElementById("xWon").className = "message";
+    var buttonClass = document.getElementById(squareClass).className;
+}
+
+function oWon() {
+    for (var i = 0; i < 9; i++) {
+        visited[i] = 1;
+    }
+    
+    document.getElementById("oWon").className = "message";
+    var buttonClass = document.getElementById(squareClass).className;
 }
