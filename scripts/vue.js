9fdb2bd3a918c9e357a21d9d3a51559a6c7d1f9b
Michał <michal.szczepaniak1999@gmail.com>
2019-03-31 21:55:25 +0200

    few changes

--- scripts/vue.js
+++ scripts/vue.js
@@ -1,5 +1,13 @@
-var counter = 0;
-var visited = [0,0,0,0,0,0,0,0,0];
+var counter = 0, startTime, endTime;
+var visited = [1,1,1,1,1,1,1,1,1];
+
+// document.getElementsByClassName("emptySquare").foreach(function(element) {
+//     element.addEventListener("click", function(event) {console.log(event)});
+// });
+
+// Array.from(document.getElementsByClassName("emptySquare")).forEach(function(element) {
+//     console.log("mleko"); element.addEventListener("click", function(event) {console.log(event)});
+// });
 
 function move(i) {
     if (visited[i-1] == 0) {
@@ -7,8 +15,8 @@ function move(i) {
 
         var squareClass = ("empty" + i);
         if (counter % 2 == 0) {
-            document.getElementById(squareClass).className = "xSquare";
-            var buttonClass = document.getElementById(squareClass).className;
+            document.getElementById(squareClass).classList.add("xSquare");
+            // var buttonClass = document.getElementById(squareClass).className;
             counter++;
         }
         else if (counter % 2 == 1) {
@@ -26,32 +34,54 @@ function move(i) {
         var won = 0;
         for (var i = 0; i < 9 && !won; i += 3){
             if ((squareClasses[i] == "xSquare" && squareClasses[i+1] == "xSquare" && squareClasses[i+2] == "xSquare") || (squareClasses[i] == "oSquare" && squareClasses[i+1] == "oSquare" && squareClasses[i+2] == "oSquare")) {
-                if (squareClasses[i] == "xSquare") xWon();
-                else if (squareClasses[i] == "oSquare") oWon();
+                if (squareClasses[i] == "xSquare") endGame(1);
+                else if (squareClasses[i] == "oSquare") endGame(2);
                 won = 1;
             }
         }
         for (var i = 0; i < 3 && !won; i++) {
             if ((squareClasses[i] == "xSquare" && squareClasses[i+3] == "xSquare" && squareClasses[i+6] == "xSquare") || (squareClasses[i] == "oSquare" && squareClasses[i+3] == "oSquare" && squareClasses[i+6] == "oSquare")) {
-                if (squareClasses[i] == "xSquare") xWon();
-                else if (squareClasses[i] == "oSquare") oWon();
+                if (squareClasses[i] == "xSquare") endGame(1);
+                else if (squareClasses[i] == "oSquare") endGame(2);
                 won = 1;
             }
         }
         if ((!won && squareClasses[0] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[8] == "xSquare") || (squareClasses[0] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[8] == "oSquare")) {
-            if (squareClasses[0] == "xSquare") xWon();
-            else if (squareClasses[0] == "oSquare") oWon();
+            if (squareClasses[0] == "xSquare") endGame(1);
+            else if (squareClasses[0] == "oSquare") endGame(2);
             won = 1;
         }
         else if ((!won && squareClasses[2] == "xSquare" && squareClasses[4] == "xSquare" && squareClasses[6] == "xSquare") || (squareClasses[2] == "oSquare" && squareClasses[4] == "oSquare" && squareClasses[6] == "oSquare")) {
-            if (squareClasses[2] == "xSquare") xWon();
-            else if (squareClasses[2] == "oSquare") oWon();
+            if (squareClasses[2] == "xSquare") endGame(1);
+            else if (squareClasses[2] == "oSquare") endGame(2);
             won = 1;
         }
 
-        if(counter == 9 && !won) draw();
+        if(counter == 9 && !won) endGame(0);
     }
 
+}
+
+function start() {
+    startTime = new Date();
+}
+
+function end() {
+    endTime = new Date();
+    var timeDiff = endTime - startTime; //in ms
+    timeDiff /= 1000; //strip the ms
+
+    return timeDiff;
+}
+
+function firstGame() {
+    document.getElementById("newGame").className = "newGame";
+    var first = document.getElementById.className;
+
+    document.getElementById("firstGame").className = "hidden";
+    var first = document.getElementById.className;
+
+    newGame();
 }
 
 function newGame() {
@@ -71,6 +101,8 @@ function newGame() {
 
     document.getElementById("oWon").className = "hidden";
     var buttonClass = document.getElementById(squareClass).className;
+
+    start();
 }
 
 function draw() {
@@ -94,4 +126,15 @@ function oWon() {
     
     document.getElementById("oWon").className = "message";
     var buttonClass = document.getElementById(squareClass).className;
+}
+
+function endGame(won) {
+    var timeDiff = end();
+
+    document.getElementById("timerText").innerText = timeDiff;
+    var timer = document.getElementById("timerText").innerText;
+    
+    if (won == 0) draw();
+    else if (won == 1) xWon();
+    else if (won == 2) oWon();
 }
