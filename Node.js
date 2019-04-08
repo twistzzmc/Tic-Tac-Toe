bbad4b7c9c6fa1118fcc4822cc3911a9c1e1c199
Michał <michal.szczepaniak1999@gmail.com>
2019-04-08 02:21:00 +0200

    for sure last heroku

--- Node.js
+++ Node.js
@@ -2,7 +2,7 @@ var app = require('./node_modules/express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 80;
 
 var url = require('url');
 var fs = require('fs');
